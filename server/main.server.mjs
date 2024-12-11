10ee87c043025d5e657a56f57a99c6d721fcff42
rmcabrera <mateussearc@gmail.com>
2024-12-11 15:35:45 -0500

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-OIFU6VWH.mjs";import"./chunk-OPNSWTYH.mjs";import"./chunk-O7HBUFUI.mjs";import"./chunk-UPIRL4TN.mjs";import"./chunk-KFBAW3U7.mjs";import"./chunk-A7GSAIJF.mjs";import"./chunk-FCXBGYVN.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
